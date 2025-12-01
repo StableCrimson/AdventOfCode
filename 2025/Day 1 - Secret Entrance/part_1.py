27502386c0d946f0678a853ac712872dd987c19e
StableCrimson <rhysethompson@gmail.com>
2025-12-01 17:49:18 -0600

    2025 Day 1

---
+++ 2025/Day 1 - Secret Entrance/part_1.py
@@ -0,0 +1,27 @@
+import os
+
+with open('input.txt') as file:
+  lines = file.readlines()
+  turns = []
+
+  for line in lines:
+    if line.startswith('L'):
+      turns.append(int(line[1:]) * -1)
+    else:
+      turns.append(int(line[1:]))
+
+dial = 50
+num_zeroes = 0
+
+for turn in turns:
+  dial += (turn % 100)
+  if dial > 99:
+    dial -= 100
+  elif dial < 0:
+    dial += 100
+
+  if dial == 0:
+    num_zeroes += 1
+
+print(dial, num_zeroes)
+
---
+++ 2025/Day 1 - Secret Entrance/part_2.py
@@ -0,0 +1,48 @@
+import os
+
+with open('input.txt') as file:
+  lines = file.readlines()
+  turns = []
+
+  for line in lines:
+    if line.startswith('L'):
+      turns.append(int(line[1:]) * -1)
+    else:
+      turns.append(int(line[1:]))
+
+dial = 50
+num_zeroes = 0
+
+for turn in turns:
+
+  start_at_zero = dial == 0
+  partial_turn = turn % 100
+
+  # Any whole turn immediately counts as passing zero
+  num_rotations = abs(turn) // 100
+  num_zeroes += num_rotations
+
+  # If turning left (-), the mod operator gives a positive number, so we need to adjust
+  # -68 % 100 = 32 <-- though the math this used in later still lines up, we need this signed value to check for wrap around
+  #  68 % 100 = 68
+  if turn < 0 and partial_turn != 0:
+    partial_turn -= 100
+
+  dial += partial_turn
+
+  # Now we handle the wrap around. Did this motion pass a zero?
+  if dial > 99:
+    dial -= 100
+    num_zeroes += 1
+  elif dial < 0:
+    dial += 100
+
+    # If we'ere starting at zero, than ANY left turn will make a wraparound
+    # So not having this check would make us double-count
+    if not start_at_zero:
+      num_zeroes += 1
+  elif dial == 0 and not start_at_zero:
+      num_zeroes += 1
+
+print(dial, num_zeroes)
+
